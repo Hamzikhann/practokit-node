4abeef4e9f4817aad5bebef35c1f03ac9cddc30d
Fareed Murtaza <fareedmurtaza91@gmail.com>
2021-06-22 10:47:41 +0500

    Modify get classesWithCourse API

--- modules/classes/class.controller.js
+++ modules/classes/class.controller.js
@@ -105,6 +105,7 @@ exports.findClasseswithCourses = (req, res) => {
             where: { isActive: 'Y' },
             include: {
                 model: Courses,
+                where: { isActive: 'Y' },
                 attributes: ['id', 'title']
             },
             attributes: { exclude: ['createdAt', 'updatedAt'] }
